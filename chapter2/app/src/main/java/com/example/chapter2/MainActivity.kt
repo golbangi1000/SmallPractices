b85763e11116a2c6fda71533bca96c7822edefcd
golbangi1000 <kenjames777@naver.com>
2023-07-07 21:54:56 +0900

    asdf
    asdf

--- chapter2/app/src/main/java/com/example/chapter2/MainActivity.kt
+++ chapter2/app/src/main/java/com/example/chapter2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.chapter2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val numberTextView = binding.numberTextView
+        val resetButton = binding.resetButton
+        val plusButton = binding.plusButton
+
+
+        var number = 0
+
+
+
+
+        resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = number.toString()
+        }
+
+        plusButton.setOnClickListener {
+            number++
+            numberTextView.text = number.toString()
+
+        }
+
     }
 }
