10de40f80b7533377f3846b1dd1dc983313788c4
golbangi1000 <kenjames777@naver.com>
2023-07-08 12:55:11 +0900

    saa
    asdf

--- chapter3/app/src/main/java/com/example/chapter3/MainActivity.kt
+++ chapter3/app/src/main/java/com/example/chapter3/MainActivity.kt
@@ -2,10 +2,68 @@ package com.example.chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.example.chapter3.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    var cmToM = true
+    var inputNumber : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+        val swapImageButton  = binding.swapImageButton
+
+
+
+
+        inputEditText.addTextChangedListener {text ->
+            inputNumber = if(text.isNullOrEmpty()){
+                0
+            } else{
+                text.toString().toInt()
+            }
+            if(cmToM){
+                var output = inputNumber.times(0.01).toString()
+                outputTextView.text = output
+            } else{
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not()
+            if(cmToM){
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else{
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
     }
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("cmToM", cmToM)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        cmToM = savedInstanceState.getBoolean("cmToM")
+        binding.inputUnitTextView.text = if(cmToM) "cm" else "m"
+        binding.outputUnitTextView.text = if(cmToM) "m" else "cm"
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+
 }
