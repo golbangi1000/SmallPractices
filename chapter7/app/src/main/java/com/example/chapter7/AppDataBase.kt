1ffdef3a3db63abdf2adac23328095a6bcc1aec0
golbangi1000 <kenjames777@naver.com>
2023-08-07 21:56:35 +0900

    awfaw
    efawef

--- chapter7/app/src/main/java/com/example/chapter7/AppDataBase.kt
+++ chapter7/app/src/main/java/com/example/chapter7/AppDataBase.kt
@@ -11,19 +11,19 @@ abstract class AppDataBase : RoomDatabase() {
     abstract fun wordDao(): WordDao
 
     companion object {
-        private var INSTANCE : AppDataBase? = null
-        fun getInstance(context : Context) : AppDataBase?{
-            if(INSTANCE == null) {
+        private var INSTANCE: AppDataBase? = null
+        fun getInstance(context: Context): AppDataBase? {
+            if (INSTANCE == null) {
                 synchronized(AppDataBase::class.java) {
-                INSTANCE = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDataBase::class.java,
-                    "app-database.db"
-                ).build()
-            }
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDataBase::class.java,
+                        "app-database.db"
+                    ).build()
+                }
             }
             return INSTANCE
         }
     }
-
 }
+
